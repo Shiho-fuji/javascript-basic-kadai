af5c578c2ae096fc6d9ecc50856d3d60a0a3e9b8
Shiho-fuji <143941894+Shiho-fuji@users.noreply.github.com>
2023-10-23 13:15:56 +0900

    Add files via upload

---
+++ kadai_014/square.js
@@ -0,0 +1,13 @@
+function square(num) {
+  return num * num;
+}
+console.log(square(10));
+
+//const squared = Math.pow(10, 2);
+//console.log(squared);
+
+//function square(no) {
+// return no ** 2
+//}
+//const number = square(10);
+//console.log(number);
